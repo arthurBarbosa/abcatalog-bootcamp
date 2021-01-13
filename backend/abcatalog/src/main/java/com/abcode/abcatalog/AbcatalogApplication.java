3c94280fc35d98bb2b1b041b97ee7d67ac75e27e
Arthur Barbosa <arthurbruno03@gmail.com>
2021-01-13 19:20:02 -0400

    Project created

---
+++ backend/abcatalog/src/main/java/com/abcode/abcatalog/AbcatalogApplication.java
@@ -0,0 +1,13 @@
+package com.abcode.abcatalog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AbcatalogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AbcatalogApplication.class, args);
+	}
+
+}
