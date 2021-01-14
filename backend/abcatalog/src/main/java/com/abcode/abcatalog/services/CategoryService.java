d6d4667f42a7fe8097ee5bc16949e67db5aa188e
Arthur Barbosa <arthurbruno03@gmail.com>
2021-01-13 22:48:47 -0400

    Category by id, exception handling

--- backend/abcatalog/src/main/java/com/abcode/abcatalog/services/CategoryService.java
+++ backend/abcatalog/src/main/java/com/abcode/abcatalog/services/CategoryService.java
@@ -3,6 +3,7 @@ package com.abcode.abcatalog.services;
 import com.abcode.abcatalog.dto.CategoryDTO;
 import com.abcode.abcatalog.entities.Category;
 import com.abcode.abcatalog.repositories.CategoryRepository;
+import com.abcode.abcatalog.services.exceptions.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -20,5 +21,11 @@ public class CategoryService {
     public List<CategoryDTO> findAll() {
         List<Category> list = repository.findAll();
         return list.stream().map(CategoryDTO::new).collect(Collectors.toList());
+    }
+
+    public CategoryDTO findById(Long id) {
+        var category = repository.findById(id);
+        var entity = category.orElseThrow(() -> new EntityNotFoundException("Entity not found"));
+        return new CategoryDTO(entity);
     }
 }
