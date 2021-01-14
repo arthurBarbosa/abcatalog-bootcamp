322fd7439b8b7d3b094d9c5bdfc08997c0c68912
Arthur Barbosa <arthurbruno03@gmail.com>
2021-01-13 21:28:32 -0400

    Configuring database h2

--- backend/abcatalog/src/main/java/com/abcode/abcatalog/entities/Category.java
+++ backend/abcatalog/src/main/java/com/abcode/abcatalog/entities/Category.java
@@ -1,14 +1,19 @@
 package com.abcode.abcatalog.entities;
 
+import javax.persistence.*;
 import java.io.Serializable;
 import java.util.Objects;
 
+@Entity
+@Table(name = "tb_category")
 public class Category implements Serializable {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
 
-    public Category(){
+    public Category() {
 
     }
 
