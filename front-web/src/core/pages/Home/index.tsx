8408954bee6ebf0d609cbd269d729a0955c55a7a
Arthur Bruno Moreira Barbosa <arthur.barbosa@ITBL000531D.itbam.org.br>
2021-01-22 15:54:13 -0400

    add: 50% home

--- front-web/src/core/pages/Home/index.tsx
+++ front-web/src/core/pages/Home/index.tsx
@@ -1,8 +1,12 @@
 import React, { Component } from "react";
+import './styles.scss';
 
 const Home = () => (
-  <div>
-    <h1>Home</h1>
+  <div className="home-container">
+    <div className="home-content">
+      <h1 className="text-title">Conheça o melhor catálago de produtos</h1>
+      <p className="text-subtitle">Ajudaremos você a encontrar os melhores produtos disponíveis no mercado.</p>
+    </div>
   </div>
 );
 
