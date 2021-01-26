779f1defcd8e498e63c308b4aadde5b605c45543
Arthur Bruno Moreira Barbosa <arthur.barbosa@ITBL000531D.itbam.org.br>
2021-01-26 11:33:34 -0400

    listing produts from api

--- front-web/src/pages/Catalog/components/ProductDetails/index.tsx
+++ front-web/src/pages/Catalog/components/ProductDetails/index.tsx
@@ -28,7 +28,7 @@ const ProductDetails = () => {
               <ProductImage className="product-details-image" />
             </div>
             <h1 className="product-details-name">Computador Desktop - Intel Core i7</h1>
-            <ProductPrice price="2.779,00" />
+            <ProductPrice price={2.779} />
           </div>
 
           <div className="col-6 product-details-card">
