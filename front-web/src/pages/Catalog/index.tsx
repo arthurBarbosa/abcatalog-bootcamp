4fa45e2e5d0ce35637437cb27819d38c5b612438
Arthur Bruno Moreira Barbosa <arthur.barbosa@ITBL000531D.itbam.org.br>
2021-01-26 17:23:33 -0400

    added pre-loading in the product list

--- front-web/src/pages/Catalog/index.tsx
+++ front-web/src/pages/Catalog/index.tsx
@@ -4,31 +4,38 @@ import axios from "axios";
 import ProductCard from "./components/ProductCard";
 import { makeRequest } from "../../core/utils/request";
 import { ProductResponse } from "../../core/types/Product";
+import ProductCardLoader from "./components/ProductCardLoader";
 import "./styles.scss";
 
 const Catalog = () => {
   const [productResponse, setProductResponse] = useState<ProductResponse>();
-
-  console.log(productResponse);
+  const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
     const params = {
       page: 0,
       linesPerPage: 12,
     };
-    makeRequest({ url: "/products", params }).then((response) =>
-      setProductResponse(response.data)
-    );
+
+    setIsLoading(true);
+    makeRequest({ url: "/products", params })
+      .then((response) => setProductResponse(response.data))
+      .finally(() => {
+        setIsLoading(false);
+      });
   }, []);
+
   return (
     <div className="catalog-container">
       <h1 className="catalog-title">Catálogo de produtos</h1>
       <div className="catalog-products">
-        {productResponse?.content.map((product) => (
-          <Link to={`/products/${product.id}`} key={product.id}>
-            <ProductCard product={product} />
-          </Link>
-        ))}
+        {isLoading ? (<ProductCardLoader />) : (
+          productResponse?.content.map((product) => (
+            <Link to={`/products/${product.id}`} key={product.id}>
+              <ProductCard product={product} />
+            </Link>
+          ))
+        )}
       </div>
     </div>
   );
