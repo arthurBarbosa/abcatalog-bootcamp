bafe6fe3f2339d189bb7679f8ff35c95d737b934
Arthur Bruno Moreira Barbosa <arthur.barbosa@ITBL000531D.itbam.org.br>
2021-02-04 16:35:21 -0400

    Configurin Route of the admin component

--- front-web/src/core/components/Navbar/index.tsx
+++ front-web/src/core/components/Navbar/index.tsx
@@ -12,17 +12,17 @@ const Navbar = () => (
     <div className="col-6 offset-2">
       <ul className="main-menu">
         <li>
-          <NavLink to="/" activeClassName="active" exact>
+          <NavLink to="/" exact>
             Home
           </NavLink>
         </li>
         <li>
-          <NavLink to="/products" activeClassName="active">
+          <NavLink to="/products" >
             Catalogo
           </NavLink>
         </li>
         <li>
-          <NavLink to="/admin" activeClassName="active">
+          <NavLink to="/admin" >
             Admin
           </NavLink>
         </li>
