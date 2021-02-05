405c097e38832e9ac8b3cfc34401e37d7e5b3cf2
Arthur Bruno Moreira Barbosa <arthur.barbosa@ITBL000531D.itbam.org.br>
2021-02-05 11:13:51 -0400

    recovering the value of the form

--- front-web/src/pages/Admin/components/Products/Form/index.tsx
+++ front-web/src/pages/Admin/components/Products/Form/index.tsx
@@ -1,16 +1,46 @@
-import React from 'react';
+import React, { useState } from 'react';
 import BaseForm from '../../BaseForm';
 import './styles.scss';
 
+type FormState = {
+  name: string;
+  price: string;
+  category: string;
+}
 const Form = () => {
+  const [formData, setFormData] = useState<FormState>({
+    name: 'Computador',
+    price: '',
+    category: ''
+  });
+
+  const handleOnChange = (event: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>) => {
+    const name = event.target.name
+    const value = event.target.value
+    setFormData(data => ({ ...data, [name]: value }))
+  }
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    console.log(formData)
+  }
+
   return (
-    <BaseForm title="CADASTRAR UM PRODUTO" >
-      <div className="row">
-        <div className="col-6">
-          <input type="text" className="form-control" />
+    <form onSubmit={handleSubmit}>
+      <BaseForm title="CADASTRAR UM PRODUTO" >
+        <div className="row">
+          <div className="col-6">
+            <input value={formData.name} name="name" type="text" className="form-control mb-5" onChange={handleOnChange} placeholder="Nome do produto" />
+            <select value={formData.category} name="category" id="" className="form-control mb-5" onChange={handleOnChange}>
+              <option value="livros">livros</option>
+              <option value="livros">Computadores</option>
+            </select>
+            <input value={formData.price} name="price" type="text" className="form-control " onChange={handleOnChange} placeholder="Preço" />
+          </div>
         </div>
-      </div>
-    </BaseForm>
+      </BaseForm>
+    </form>
   )
 }
 export default Form;
