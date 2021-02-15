59ad915b304d027dfb83aa4822c08bda94a90131
Arthur Barbosa <arthurbruno03@gmail.com>
2021-02-15 14:41:35 -0400

    init implementation oauth

--- backend/abcatalog/src/main/java/com/abcode/abcatalog/services/UserService.java
+++ backend/abcatalog/src/main/java/com/abcode/abcatalog/services/UserService.java
@@ -9,11 +9,16 @@ import com.abcode.abcatalog.repositories.RoleRepository;
 import com.abcode.abcatalog.repositories.UserRepository;
 import com.abcode.abcatalog.services.exceptions.DatabaseException;
 import com.abcode.abcatalog.services.exceptions.ResourceNotFoundException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -21,7 +26,9 @@ import org.springframework.transaction.annotation.Transactional;
 import javax.persistence.EntityNotFoundException;
 
 @Service
-public class UserService {
+public class UserService implements UserDetailsService {
+
+    private static Logger logger = LoggerFactory.getLogger(UserService.class);
 
     @Autowired
     private UserRepository repository;
@@ -87,5 +94,16 @@ public class UserService {
             var role = roleRepository.getOne(roleDTO.getId());
             entity.getRoles().add(role);
         }
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = repository.findByEmail(username);
+        if (user == null) {
+            logger.error("User not found: " + username);
+            throw new UsernameNotFoundException("Email not found");
+        }
+        logger.info("User found: " + username);
+        return user;
     }
 }
