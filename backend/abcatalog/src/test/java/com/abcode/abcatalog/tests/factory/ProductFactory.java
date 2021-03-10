c5f8811c1497e22014214f9fd2836e46e47b1048
Arthur Bruno Moreira Barbosa <arthur.barbosa@ITBL000531D.itbam.org.br>
2021-03-10 15:15:50 -0400

    implementing test web findById with existing id and nonExistind id

--- backend/abcatalog/src/test/java/com/abcode/abcatalog/tests/factory/ProductFactory.java
+++ backend/abcatalog/src/test/java/com/abcode/abcatalog/tests/factory/ProductFactory.java
@@ -1,18 +1,29 @@
 package com.abcode.abcatalog.tests.factory;
 
 import com.abcode.abcatalog.dto.ProductDTO;
+import com.abcode.abcatalog.entities.Category;
 import com.abcode.abcatalog.entities.Product;
+import org.checkerframework.checker.units.qual.C;
 
 import java.time.Instant;
 
 public class ProductFactory {
 
     public static Product createProduct() {
-        return new Product(null, "Phone", "Good Phone", 800.0, "http://img.com/img.png", Instant.parse("2021-10-20T03:00:00Z"));
+        Product product = new Product(null, "Phone", "Good Phone", 800.0, "http://img.com/img.png", Instant.parse("2021-10-20T03:00:00Z"));
+        product.getCategories().add(new Category(1L, null));
+        return product;
     }
 
     public static ProductDTO createProductDTO() {
-        return new ProductDTO(createProduct());
+        Product product = createProduct();
+        return new ProductDTO(product, product.getCategories());
+    }
+
+    public static ProductDTO createProductDTO(Long id) {
+        ProductDTO dto = createProductDTO();
+        dto.setId(id);
+        return dto;
     }
 
 }
