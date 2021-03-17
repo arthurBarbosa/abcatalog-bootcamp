f29c9f6c2fbc7cbe0250f138a16e16f47a1d915a
Arthur Bruno Moreira Barbosa <arthur.barbosa@ITBL000531D.itbam.org.br>
2021-03-17 15:32:05 -0400

    Make login and save user session data

--- front-web/src/pages/Auth/components/Login/index.tsx
+++ front-web/src/pages/Auth/components/Login/index.tsx
@@ -1,34 +1,53 @@
 import ButtonIcon from 'core/components/ButtonIcon';
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState } from 'react';
+import { Link, useHistory } from 'react-router-dom';
 import { useForm } from 'react-hook-form';
 import AuthCard from '../Card'
 import './styles.scss';
+import { makeLogin } from 'core/utils/request';
+import { saveSessionData } from 'core/utils/auth';
 
 type FormData = {
-  email: string;
+  username: string;
   password: string;
 }
 
 const Login = () => {
-  const { register, handleSubmit } = useForm<FormData>(); // initialize the hook
+  const { register, handleSubmit } = useForm<FormData>();
+  const [hasError, sethasError] = useState(false);
+  const history = useHistory();
+
   const onSubmit = (data: FormData) => {
-    console.log(data)
+    makeLogin(data)
+      .then(response => {
+        sethasError(false);
+        saveSessionData(response.data);
+        history.push('/admin');
+      })
+      .catch(() => {
+        sethasError(true);
+      })
   }
+
   return (
     <AuthCard title="login" >
+      {hasError && (
+        <div className="alert alert-danger mt-5">
+          Usuário ou senha inválidos
+        </div>
+      )}
       <form className="login-form" onSubmit={handleSubmit(onSubmit)}>
         <input
           type="email"
           className="form-control input-base margin-bottom-30"
           placeholder="Email"
-          name="email" ref={register} />
+          name="username" ref={register({ required: true })} />
 
         <input
           type="password"
           className="form-control input-base"
           placeholder="Senha"
-          name="password" ref={register} />
+          name="password" ref={register({ required: true })} />
 
         <Link to="/admin/auth/recover" className="login-link-recover">
           Esqueci a senha?
