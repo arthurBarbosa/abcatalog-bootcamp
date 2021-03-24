c3ce9057cc0920ab27b3bfd5c602f25d82f0f87d
Arthur Bruno Moreira Barbosa <arthur.barbosa@ITBL000531D.itbam.org.br>
2021-03-24 17:33:37 -0400

    Init listing product admin

---
+++ front-web/src/pages/Admin/components/Products/Card/index.tsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import './styles.scss'
+
+const Card = () => {
+  return (
+    <div className="card-base product-card-admin">
+      <div className="row">
+        <div className="col-2 text-center border-right py-3">
+          <img className="product-card-image-admin" src="https://raw.githubusercontent.com/devsuperior/dscatalog-resources/master/backend/img/2-big.jpg" alt="Produto test" />
+        </div>
+
+        <div className="col-7 py-3">
+          <h3 className="product-card-name-admin">Computador i7</h3>
+        </div>
+
+        <div className="col-3 py-3">
+          <h1>Ações</h1>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Card;
--- front-web/src/pages/Admin/components/Products/List/index.tsx
+++ front-web/src/pages/Admin/components/Products/List/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { useHistory } from 'react-router-dom';
+import Card from '../Card';
 
 const List = () => {
 
@@ -15,6 +16,10 @@ const List = () => {
       <button className="btn btn-primary btn-lg" onClick={handleCreate}>
         ADICIONAR
       </button>
+      <div className="admin-list-container">
+        <Card />
+        <Card />
+      </div>
     </div>
   )
 }
