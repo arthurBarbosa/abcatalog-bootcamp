a04753bd373e394cbe52d33b780934626438f33c
Arthur Bruno Moreira Barbosa <arthur.barbosa@ITBL000531D.itbam.org.br>
2021-03-30 17:10:11 -0400

    Implementing filter by name in Catalog products

---
+++ front-web/src/core/components/ProductFilters/index.tsx
@@ -0,0 +1,65 @@
+import React, { useEffect, useState } from 'react';
+import { ReactComponent as SearchIcon } from 'core/assets/images/search-icon.svg';
+import Select from 'react-select';
+import './styles.scss';
+import { makeRequest } from 'core/utils/request';
+import { Category } from 'core/types/Product';
+
+export type FilterForm = {
+  name?: string;
+  categoryId?: number
+}
+
+type Props = {
+  onSearch: (filter: FilterForm) => void;
+}
+
+const ProductFilters = ({ onSearch }: Props) => {
+
+  const [isLoadingCategories, setIsLoadingCategories] = useState(false);
+  const [categories, setCategories] = useState<Category[]>([]);
+  const [name, setName] = useState('');
+
+  useEffect(() => {
+    setIsLoadingCategories(true)
+    makeRequest({ url: '/categories' })
+      .then(response => setCategories(response.data.content))
+      .finally(() => setIsLoadingCategories(false));
+  }, []);
+
+  const handleChangeName = (name: string) => {
+    setName(name);
+    onSearch({ name });
+  }
+
+  return (
+    <div className="card-base product-filters-container">
+      <div className="input-search">
+        <input
+          type="text"
+          value={name}
+          className="form-control"
+          placeholder="Pesquisar Produto"
+          onChange={event => handleChangeName(event.target.value)}
+        />
+        <SearchIcon />
+      </div>
+      <Select
+        name="categories"
+        isLoading={isLoadingCategories}
+        options={categories}
+        getOptionLabel={(option: Category) => option.name}
+        getOptionValue={(option: Category) => String(option.id)}
+        className="filter-select-container"
+        classNamePrefix="product-categories-select"
+        placeholder="Filtrar por Categorias"
+        inputId="categories"
+      />
+      <button className="btn btn-outline-secondary border-radius-10">
+        LIMPAR FILTRO
+      </button>
+    </div>
+  )
+}
+
+export default ProductFilters;
--- front-web/src/pages/Catalog/index.tsx
+++ front-web/src/pages/Catalog/index.tsx
@@ -1,10 +1,11 @@
-import React, { useEffect, useState } from "react";
+import React, { useCallback, useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import ProductCard from "./components/ProductCard";
 import { makeRequest } from "core/utils/request";
 import { ProductResponse } from "core/types/Product";
 import ProductCardLoader from "./components/Loaders/ProductCardLoader";
 import Pagination from "core/components/Pagination";
+import ProductFilters, { FilterForm } from 'core/components/ProductFilters';
 import "./styles.scss";
 
 const Catalog = () => {
@@ -12,10 +13,12 @@ const Catalog = () => {
   const [isLoading, setIsLoading] = useState(false);
   const [activePage, setActivePage] = useState(0);
 
-  useEffect(() => {
+  const getProducts = useCallback((filter?: FilterForm) => {
     const params = {
       page: activePage,
       linesPerPage: 12,
+      name: filter?.name,
+      categoryId: filter?.categoryId
     };
 
     setIsLoading(true);
@@ -24,11 +27,20 @@ const Catalog = () => {
       .finally(() => {
         setIsLoading(false);
       });
-  }, [activePage]);
+  }, [activePage])
+
+  useEffect(() => {
+    getProducts()
+  }, [getProducts]);
 
   return (
     <div className="catalog-container">
-      <h1 className="catalog-title">Catálogo de produtos</h1>
+      <div className="d-flex justify-content-between">
+        <h1 className="catalog-title">
+          Catálogo de produtos
+      </h1>
+        <ProductFilters onSearch={filter => getProducts(filter)} />
+      </div>
       <div className="catalog-products">
         {isLoading ? (
           <ProductCardLoader />
