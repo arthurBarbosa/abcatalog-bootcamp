15fb51aeca416ddc78f73b2c9368bba82f910c42
Arthur Bruno Moreira Barbosa <arthur.barbosa@ITBL000531D.itbam.org.br>
2021-03-31 11:41:43 -0400

    Fix pagination ProductFilters

--- front-web/src/pages/Catalog/index.tsx
+++ front-web/src/pages/Catalog/index.tsx
@@ -2,23 +2,25 @@ import React, { useCallback, useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import ProductCard from "./components/ProductCard";
 import { makeRequest } from "core/utils/request";
-import { ProductResponse } from "core/types/Product";
+import { Category, ProductResponse } from "core/types/Product";
 import ProductCardLoader from "./components/Loaders/ProductCardLoader";
 import Pagination from "core/components/Pagination";
-import ProductFilters, { FilterForm } from 'core/components/ProductFilters';
+import ProductFilters from 'core/components/ProductFilters';
 import "./styles.scss";
 
 const Catalog = () => {
   const [productResponse, setProductResponse] = useState<ProductResponse>();
   const [isLoading, setIsLoading] = useState(false);
   const [activePage, setActivePage] = useState(0);
+  const [name, setName] = useState('');
+  const [category, setCategory] = useState<Category>();
 
-  const getProducts = useCallback((filter?: FilterForm) => {
+  const getProducts = useCallback(() => {
     const params = {
       page: activePage,
       linesPerPage: 12,
-      name: filter?.name,
-      categoryId: filter?.categoryId
+      name,
+      categoryId: category?.id
     };
 
     setIsLoading(true);
@@ -27,11 +29,30 @@ const Catalog = () => {
       .finally(() => {
         setIsLoading(false);
       });
-  }, [activePage])
+  }, [activePage, name, category])
 
   useEffect(() => {
     getProducts()
   }, [getProducts]);
+
+  const handleChangeName = (name: string) => {
+    setActivePage(0)
+    setName(name);
+
+  }
+
+  const handleChangeCategory = (category: Category) => {
+    setActivePage(0)
+    setCategory(category);
+
+  }
+
+  const clearFilters = () => {
+    setActivePage(0)
+    setCategory(undefined)
+    setName('')
+
+  }
 
   return (
     <div className="catalog-container">
@@ -39,7 +60,12 @@ const Catalog = () => {
         <h1 className="catalog-title">
           Catálogo de produtos
       </h1>
-        <ProductFilters onSearch={filter => getProducts(filter)} />
+        <ProductFilters
+          name={name}
+          category={category}
+          handleChangeCategory={handleChangeCategory}
+          handleChangeName={handleChangeName}
+          clearFilters={clearFilters} />
       </div>
       <div className="catalog-products">
         {isLoading ? (
