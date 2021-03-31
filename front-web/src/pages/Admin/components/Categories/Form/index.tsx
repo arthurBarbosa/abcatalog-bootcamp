8eaaa1f0136808b7f87d9449e04995e2da2f5baa
Arthur Bruno Moreira Barbosa <arthur.barbosa@ITBL000531D.itbam.org.br>
2021-03-31 16:35:08 -0400

    Post Categories

---
+++ front-web/src/pages/Admin/components/Categories/Form/index.tsx
@@ -0,0 +1,77 @@
+import { makePrivateRequest, makeRequest } from 'core/utils/request';
+import { url } from 'inspector';
+import React, { useEffect } from 'react';
+import { useForm } from 'react-hook-form';
+import { useHistory, useParams } from 'react-router';
+import { toast } from 'react-toastify';
+import BaseForm from '../../BaseForm';
+
+export type FormState = {
+  name: string;
+}
+
+type ParamsType = {
+  categoryId: string;
+}
+
+const Form = () => {
+  const { register, handleSubmit, errors, setValue, control } = useForm<FormState>();
+  const history = useHistory();
+  const { categoryId } = useParams<ParamsType>();
+  const isEditing = categoryId !== 'create';
+  const formTitle = isEditing ? 'Editar Categoria' : 'Cadastrar Categoria'
+
+  useEffect(() => {
+    if (isEditing) {
+      makeRequest({ url: `/categories/${categoryId}` })
+        .then(response => {
+          setValue('name', response.data.name)
+        });
+    }
+  }, [categoryId, isEditing, setValue]);
+
+  const onSubmit = (data: FormState) => {
+    makePrivateRequest({
+      url: isEditing ? `/categories/${categoryId}` : '/categories',
+      method: isEditing ? 'PUT' : 'POST',
+      data
+    })
+      .then(() => {
+        toast.info('Categoria cadastrada com sucesso.');
+        history.push('/admin/categories')
+      })
+      .catch(() => {
+        toast.error('Erro ao salvar categoria.');
+      })
+  }
+
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <BaseForm title={formTitle}>
+        <div className="row">
+          <div className="col-12">
+            <div className="margin-bottom-30">
+              <input
+                ref={register({
+                  required: 'Campo obrigatório',
+                  minLength: { value: 3, message: 'Campo deve ter no mínimo 3 caracteres' },
+                  maxLength: { value: 60, message: 'Campo deve ter no máximo 60 caracteres' }
+                })
+
+                }
+                type="text" name="name" className="form-control" placeholder="Nome da categoria" />
+
+              {errors.name && (
+                <div className="invalid-feedback d-block">
+                  {errors.name.message}
+                </div>
+              )}
+            </div>
+          </div>
+        </div>
+      </BaseForm>
+    </form>
+  )
+}
+
+export default Form;
--- front-web/src/pages/Admin/components/Categories/index.tsx
+++ front-web/src/pages/Admin/components/Categories/index.tsx
@@ -1,12 +1,17 @@
 import React from 'react';
 import { Route, Switch } from 'react-router';
-import List from '../Categories/List'
+import List from '../Categories/List';
+import Form from '../Categories/Form';
 const Categories = () => {
   return (
     <div>
       <Switch>
         <Route path="/admin/categories" exact>
           <List />
+        </Route>
+
+        <Route path="/admin/categories/:categoryId">
+          <Form />
         </Route>
       </Switch>
     </div>
