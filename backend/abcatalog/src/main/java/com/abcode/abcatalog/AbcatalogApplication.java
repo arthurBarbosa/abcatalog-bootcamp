b438fa8bb2f15a3d2e7b93f90dda488c1e369a1a
Arthur Barbosa <arthurbruno03@gmail.com>
2021-05-13 15:46:05 -0400

    First upload test

--- backend/abcatalog/src/main/java/com/abcode/abcatalog/AbcatalogApplication.java
+++ backend/abcatalog/src/main/java/com/abcode/abcatalog/AbcatalogApplication.java
@@ -1,13 +1,23 @@
 package com.abcode.abcatalog;
 
+import com.abcode.abcatalog.services.S3Service;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class AbcatalogApplication {
+public class AbcatalogApplication implements CommandLineRunner {
 
-	public static void main(String[] args) {
-		SpringApplication.run(AbcatalogApplication.class, args);
-	}
+    @Autowired
+    private S3Service s3Service;
 
+    public static void main(String[] args) {
+        SpringApplication.run(AbcatalogApplication.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        s3Service.uploadFile("/home/arthurbarbosa/Imagens/print.png");
+    }
 }
